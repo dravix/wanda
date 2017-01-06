81804a9f3a02a8f762297b7d11a969da5577facc
David Osorio <dravix@gmail.com>
2017-01-05 18:25:25 -0600

    Se agregaron las familias y unidades en productos

---
+++ views/familias/update.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * /var/www/html/wanda/runtime/giiant/fcd70a9bfdf8de75128d795dfc948a74
+ *
+ * @package default
+ */
+
+
+use yii\helpers\Html;
+
+/**
+ *
+ * @var yii\web\View $this
+ * @var app\models\Familias $model
+ */
+$this->title = 'Familias' . " " . $model->id . ', ' . 'Edit';
+$this->params['breadcrumbs'][] = ['label' => 'Familias', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => (string)$model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = 'Edit';
+?>
+<div class="giiant-crud familias-update">
+
+    <h1>
+        <?php echo 'Familias' ?>
+        <small>
+                        <?php echo $model->id ?>
+        </small>
+    </h1>
+
+    <div class="crud-navigation">
+        <?php echo Html::a('<span class="glyphicon glyphicon-file"></span> ' . 'View', ['view', 'id' => $model->id], ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <hr />
+
+    <?php echo $this->render('_form', [
+		'model' => $model,
+	]); ?>
+
+</div>
---
+++ views/familias/view.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * /var/www/html/wanda/runtime/giiant/d4b4964a63cc95065fa0ae19074007ee
+ *
+ * @package default
+ */
+
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\GridView;
+use yii\widgets\DetailView;
+use yii\widgets\Pjax;
+use dmstr\bootstrap\Tabs;
+
+/**
+ *
+ * @var yii\web\View $this
+ * @var app\models\Familias $model
+ */
+$copyParams = $model->attributes;
+
+$this->title = 'Familias';
+$this->params['breadcrumbs'][] = ['label' => 'Familias', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => (string)$model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = 'View';
+?>
+<div class="giiant-crud familias-view">
+
+    <!-- flash message -->
+    <?php if (\Yii::$app->session->getFlash('deleteError') !== null) : ?>
+        <span class="alert alert-info alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span></button>
+            <?php echo \Yii::$app->session->getFlash('deleteError') ?>
+        </span>
+    <?php endif; ?>
+
+    <h1>
+        <?php echo 'Familias' ?>
+        <small>
+            <?php echo $model->id ?>
+        </small>
+    </h1>
+
+
+    <div class="clearfix crud-navigation">
+
+        <!-- menu buttons -->
+        <div class='pull-left'>
+            <?php echo Html::a(
+	'<span class="glyphicon glyphicon-pencil"></span> ' . 'Edit',
+	[ 'update', 'id' => $model->id],
+	['class' => 'btn btn-info']) ?>
+
+            <?php echo Html::a(
+	'<span class="glyphicon glyphicon-copy"></span> ' . 'Copy',
+	['create', 'id' => $model->id, 'Familias'=>$copyParams],
+	['class' => 'btn btn-success']) ?>
+
+            <?php echo Html::a(
+	'<span class="glyphicon glyphicon-plus"></span> ' . 'New',
+	['create'],
+	['class' => 'btn btn-success']) ?>
+        </div>
+
+        <div class="pull-right">
+            <?php echo Html::a('<span class="glyphicon glyphicon-list"></span> '
+	. 'Full list', ['index'], ['class'=>'btn btn-default']) ?>
+        </div>
+
+    </div>
+
+    <hr />
+
+    <?php $this->beginBlock('app\models\Familias'); ?>
+
+
+    <?php echo DetailView::widget([
+		'model' => $model,
+		'attributes' => [
+			'nombre',
+			'departamento',
+		],
+	]); ?>
+
+
+    <hr/>
+
+    <?php echo Html::a('<span class="glyphicon glyphicon-trash"></span> ' . 'Delete', ['delete', 'id' => $model->id],
+	[
+		'class' => 'btn btn-danger',
+		'data-confirm' => '' . 'Are you sure to delete this item?' . '',
+		'data-method' => 'post',
+	]); ?>
+    <?php $this->endBlock(); ?>
+
+
+
+    <?php echo Tabs::widget(
+	[
+		'id' => 'relation-tabs',
+		'encodeLabels' => false,
+		'items' => [
+			[
+				'label'   => '<b class=""># '.$model->id.'</b>',
+				'content' => $this->blocks['app\models\Familias'],
+				'active'  => true,
+			],
+		]
+	]
+);
+?>
+</div>
