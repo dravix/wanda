77bd7803ed8437bf7df6d29bb7757ffbee493ede
David Osorio <dravix@gmail.com>
2023-04-28 12:03:16 -0600

    Remove all php related

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=pyventa',
-    'username' => 'pyventa',
-    'password' => 'damasco',
-    'charset' => 'utf8',
-];
